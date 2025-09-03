97e7f3fb706dc0aa35a1eb097b580f3153c5d013
ja121 <37303516+ja121@users.noreply.github.com>
2025-09-03 17:06:05 +0300

    Update __init__.py

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,1 +1,6 @@
 
+"""Machine learning models for glucose prediction"""
+from .xgboost_predictor import EnhancedGlucosePredictor
+from .complication_predictor import ComplicationPredictor
+
+__all__ = ['EnhancedGlucosePredictor', 'ComplicationPredictor']
